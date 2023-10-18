1d2201d2826205ce927542daec0ece73064a4829
nada <nada50tarek@gmail.com>
2023-10-18 15:30:21 +0300

    make app portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:image_generator/views/home_view.dart';
 import 'package:image_generator/views/splash_screen.dart';
-
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+  ]).then((value) => runApp(const MyApp()));
+
   runApp(const MyApp());
 }
 
